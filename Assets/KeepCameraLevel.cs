4948b784b10f89e38e2d72d0a24820c99e814c71
Raisybear <elias.spycher@gmail.com>
2024-05-17 10:38:29 +0200

    er
    e

--- Assets/KeepCameraLevel.cs
+++ Assets/KeepCameraLevel.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KeepCameraLevel : MonoBehaviour
 {
-    // Update is called once per frame
     void LateUpdate()
     {
         // Setzt die Rotation der Kamera zurück, sodass sie immer horizontal bleibt
