319331614ec2687a203ab0318a2171f51417a67a
robinsacher <110891559+robinsacher@users.noreply.github.com>
2024-05-31 10:08:14 +0200

    Finish + level 2
    Finish + level 2

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public Text deathText;
+    public TMP_Text deathText;
     public static int deathCount;
 
     void Start()
